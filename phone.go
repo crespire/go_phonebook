af65e1e78c3ddc3c3bb20612b1c8023b516b4dfe
crespire <li.simmon@gmail.com>
2022-10-31 16:12:15 -0400

    feat: add entry if not found

--- phone.go
+++ phone.go
@@ -18,18 +18,34 @@ func main() {
 	fmt.Println("Welcome to a simple phonebook!")
 	var phonebook = make(map[string]string)
 	var response string
+	var number, name, lookup string
 
 	for {
 		fmt.Println("What would you like to do?")
 		fmt.Println("> [l]ist")
+		fmt.Println("> [f]ind")
 		fmt.Println("> [a]dd/update")
 		fmt.Println("> [q]uit")
 		fmt.Scanln(&response)
 
 		if response == "l" {
 			list(phonebook)
+		} else if response == "f" {
+			fmt.Print("Enter a name to find: ")
+			fmt.Scanln(&lookup)
+			if _, exist := phonebook[lookup]; exist {
+				fmt.Printf("Found %v's number %v\n", lookup, phonebook[lookup])
+			} else {
+				fmt.Printf("Sorry, %v is not an entry.\n", lookup)
+				fmt.Print("Do you want to add them? (y/n) ")
+				fmt.Scan(&response)
+				if response == "y" {
+					fmt.Print("Enter content for entry: ")
+					fmt.Scan(&number)
+					phonebook[lookup] = number
+				}
+			}
 		} else if response == "a" {
-			var name, number string
 			fmt.Println("You can enter a previous entry to update it.")
 			fmt.Print("Enter a name for the entry: ")
 			fmt.Scanln(&name)
